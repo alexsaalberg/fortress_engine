c3e6eecf19cddc9ab9b87120ae9178576d503447
Alex Saalberg <saalberg.software@gmail.com>
2018-12-10 20:11:21 -0800

    Add new shaders to render PolyVox Ambient Occlusion.

---
+++ resources/shaders/voxel_AOCalc_frag.glsl
@@ -0,0 +1,54 @@
+#version 330 core
+
+in vec3 fPosition_World;
+in vec4 block_color;
+in float fAmbientOcclusion;
+
+//to send the color to a frame buffer
+layout(location = 0) out vec4 color;
+
+//uniform sampler2D Texture0;
+
+uniform mat4 P;
+uniform mat4 V;
+uniform mat4 trans_inv_V;
+
+/*LIGHTING*/
+uniform vec3 eyePosition;
+uniform vec3 directionTowardsLight;
+
+uniform vec3 mDiffusionCoefficient;
+uniform vec3 mAmbientCoefficient;
+uniform vec3 mSpecularCoefficient;
+
+uniform float mSpecularAlpha;
+
+void main()
+{
+    vec4 blockColor_withAmbient = vec4(block_color.xyz * fAmbientOcclusion, block_color.w);
+
+    vec3 fNormal_world = cross(dFdy(fPosition_World.xyz), dFdx(fPosition_World.xyz));
+    fNormal_world = normalize(fNormal_world);
+    
+    //vec4 fNormal_camera_nohomo = P * V * vec4(fNormal_world, 0.0f);
+    vec4 fNormal_camera_nohomo = P * trans_inv_V * vec4(fNormal_world, 0.0f);
+    vec3 fNormal_camera = fNormal_camera_nohomo.xyz / fNormal_camera_nohomo.w;
+    fNormal_camera = normalize(fNormal_camera);
+    fNormal_camera = abs(fNormal_camera);
+    
+    vec3 directionTowardsEye = normalize(eyePosition - fPosition_World);
+    
+    vec3 L = normalize(directionTowardsLight);
+    vec3 N = normalize(abs(fNormal_world));
+    vec3 H = normalize(directionTowardsLight + directionTowardsEye);
+    
+    vec3 lightColor = vec3(0.0f);
+    
+    vec3 ambientLight = mAmbientCoefficient;
+    vec3 diffusionLight = blockColor_withAmbient.xyz * clamp(dot(N, L), 0.0, 1.0);
+    vec3 specularLight = mSpecularCoefficient * pow( clamp(dot(H, N), 0.0, 1.0), mSpecularAlpha);
+    
+    lightColor = ambientLight + diffusionLight + specularLight;
+    
+    color = vec4(lightColor, blockColor_withAmbient.w);
+}
---
+++ resources/shaders/voxel_AOCalc_vert.glsl
@@ -0,0 +1,85 @@
+#version  330 core
+
+uniform mat4 P;
+uniform mat4 V;
+uniform mat4 M;
+
+layout(location = 0) in vec3 vPosition;
+layout(location = 1) in int ambientOcclusionAmount;
+layout(location = 3) in int block_type;
+
+const int BLOCK_AIR = 0;
+const int BLOCK_GRASS = 1;
+const int BLOCK_STONE = 2;
+const int BLOCK_YELLOW = 3;
+const int BLOCK_ORANGE = 4;
+const int BLOCK_RED = 5;
+const int BLOCK_VIOLET = 6;
+const int BLOCK_BLUE = 7;
+const int BLOCK_BLACK = 8;
+const int BLOCK_WHITE = 9;
+const int BLOCK_REDOUTLINE = 10;
+
+const float aovalues[] = float[](0.15, 0.6, 0.8, 1.0);
+
+out vec3 fPosition_World;
+out vec4 block_color;
+out float fAmbientOcclusion;
+
+void main()
+{
+    gl_Position = P * V * M * vec4(vPosition, 1.0); //Move position to camera space
+    fPosition_World = (M * vec4(vPosition, 1.0f)).xyz;
+
+    fAmbientOcclusion=aovalues[ambientOcclusionAmount];
+
+	switch(block_type) {
+		case BLOCK_GRASS:
+			block_color = vec4(0.1f, 0.9f, 0.1f, 1.0f);
+			break;
+		case BLOCK_STONE:
+			block_color = vec4(0.5f, 0.5f, 0.5f, 1.0f);
+			break;
+		case BLOCK_YELLOW:
+			block_color = vec4(1.0f, 1.0f, 0.0f, 1.0f);
+			break;
+		case BLOCK_ORANGE:
+			block_color = vec4(1.0f, 0.5f, 0.0f, 1.0f);
+			break;
+		case BLOCK_RED:
+			block_color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+			break;
+		case BLOCK_VIOLET:
+			block_color = vec4(0.7f, 0.2f, 0.7f, 1.0f);
+			break;
+		case BLOCK_BLUE:
+			block_color = vec4(0.0f, 0.0f, 1.0f, 1.0f);
+			break;
+		case BLOCK_BLACK:
+			block_color = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+			break;
+		case BLOCK_WHITE:
+			block_color = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+			break;
+		case BLOCK_REDOUTLINE:
+			block_color = vec4(1.0f, 0.0f, 0.0f, 0.5f);
+			break;
+		default:
+			block_color = vec4(0.0f, 1.0f, 1.0f, 1.0f);
+			break;
+	}
+/*
+    if(block_type == BLOCK_GRASS) {
+        block_color = vec4(0.0f, 0.0f, 1.0f, 1.0f);
+    } else if (block_type == BLOCK_STONE) {
+        block_color = vec4(0.2f, 0.9f, 0.2f, 1.0f);
+    } else if (block_type == BLOCK_REDOUTLINE) {
+        block_color = vec4(1.0f, 0.0, 0.0f, 0.5f);
+    } else if
+	
+	} else {
+        block_color = vec4(0.2f, 0.3f, 0.3f, 1.0f);
+    }
+	*/
+}
+
